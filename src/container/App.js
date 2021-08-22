7071c98ca1c2aa9e155e989d914674a08420f51b
Olena <zolina.alena@gmail.com>
2021-08-22 15:10:54 +0300

    added basic app structure

--- src/container/App.js
+++ src/container/App.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <div>
+                <p>lorem hello</p>
+            </div>
+        </>
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './container/App'
 
-// const el = React.createElement("h1", null, "hello");
-const el1 = (
-    <div>
-        <h1>Hello world 1</h1>
-        <h1>Hello world 2</h1>
-    </div>
-)
-
-ReactDOM.render(el1, document.getElementById('root'))
-
-console.log(el1)
+ReactDOM.render(<App />, document.getElementById('root'))
