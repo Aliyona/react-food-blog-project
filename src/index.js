f1710e469e7f82f811457013c957c07064a396fe
Olena <zolina.alena@gmail.com>
2021-08-22 14:01:27 +0300

    prettier formating installed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+// const el = React.createElement("h1", null, "hello");
+const el1 = (
+    <div>
+        <h1>Hello world 1</h1>
+        <h1>Hello world 2</h1>
+    </div>
+)
+
+ReactDOM.render(el1, document.getElementById('root'))
+
+console.log(el1)
